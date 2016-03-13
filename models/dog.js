56decf8b09d1725aef58ec003a193fb2b615c2d2
Monk <monq.wellington@gmail.com>
2016-03-13 13:53:49 -0700

    create dog model.

---
+++ models/dog.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var dogSchema = new Schema({
+  name: String,
+  breed: String,
+  color: String,
+  age: Number,
+  size: ["small", "medium", "large"],
+  location: String,
+  image: String,
+  reward: String,
+  phoneNumber: String,
+  description: String,
+  lost: Boolean
+});
