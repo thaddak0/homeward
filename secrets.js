48032b242722dd68395ae2f69393f6ef199184c8
thaddak0 <annabellethaddeus@gmail.com>
2016-03-17 21:33:05 -0700

    changes

--- secrets.js
+++ secrets.js
@@ -1,7 +1,7 @@
 const OAUTH = {
   fb: {
-    clientID: process.env.FB_CLIENT || "548166635351424",
-    clientSecret: process.env.FB_SECRET || "5bcca470f23d7d7acab4c5f6a522b0b5"
+    clientID: process.env.FB_CLIENT,
+    clientSecret: process.env.FB_SECRET
   },
 };
 
